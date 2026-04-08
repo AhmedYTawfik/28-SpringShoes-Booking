70cab0e766f1054154b599fbe8b57f78bfdddc21
Ali-Hosam <ahea2552003@gmail.com>
2026-04-08 02:23:53 +0200

    feat(invoice-service): add get invoice details with applied discounts S5-F8 (55-6777)

--- invoice-service/src/main/java/com/team28/booking/invoice/repository/InvoiceRepository.java
+++ invoice-service/src/main/java/com/team28/booking/invoice/repository/InvoiceRepository.java
@@ -1,10 +1,23 @@
 package com.team28.booking.invoice.repository;
 
-import com.team28.booking.invoice.model.Invoice;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import com.team28.booking.invoice.model.Invoice;
 
 @Repository
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
+    @Query("""
+        SELECT DISTINCT i
+        FROM Invoice i
+        LEFT JOIN FETCH i.invoiceDiscounts id
+        LEFT JOIN FETCH id.discount d
+        WHERE i.id = :invoiceId
+        """)
+    Optional<Invoice> findByIdWithDiscounts(@Param("invoiceId") Long invoiceId);
     
 }
