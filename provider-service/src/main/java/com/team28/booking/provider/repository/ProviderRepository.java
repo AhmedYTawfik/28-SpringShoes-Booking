ffc31495a44f1ffc0c166db2e4871816b795b894
zoatel <yaseenashraf@protonmail.com>
2026-04-09 23:52:49 +0200

    feat(provider-service): add provider and certification CRUD (68-38171)

---
+++ provider-service/src/main/java/com/team28/booking/provider/repository/ProviderRepository.java
@@ -0,0 +1,9 @@
+package com.team28.booking.provider.repository;
+
+import com.team28.booking.provider.model.Provider;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProviderRepository extends JpaRepository<Provider, Long> {
+}
