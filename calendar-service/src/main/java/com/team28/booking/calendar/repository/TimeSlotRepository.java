9f594d8c10d71eaa3244183371d21f43461f3533
Ahmed Yasser <ahmedyasser55577@gmail.com>
2026-04-10 14:39:22 +0200

    fix(calendar-service): added removed line in repos (55-8947)

--- calendar-service/src/main/java/com/team28/booking/calendar/repository/TimeSlotRepository.java
+++ calendar-service/src/main/java/com/team28/booking/calendar/repository/TimeSlotRepository.java
@@ -4,11 +4,11 @@ import com.team28.booking.calendar.model.TimeSlot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.Optional;
 
-@Repository
+@RepositoryRestResource(exported=false)
 public interface TimeSlotRepository extends JpaRepository<TimeSlot, Long> {
 
     @Query(value = "SELECT COUNT(*) FROM providers WHERE id = :providerId", nativeQuery = true)
