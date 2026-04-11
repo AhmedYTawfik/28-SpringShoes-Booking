1e19719f0a18293b4a38d415e30f827e90712cfc
TheSant0x <mahmoud7103@gmail.com>
2026-04-11 21:16:02 +0200

    chore(user-service): restore deleted tests (55-24394)

---
+++ user-service/src/test/java/com/team28/booking/user/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team28.booking.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
