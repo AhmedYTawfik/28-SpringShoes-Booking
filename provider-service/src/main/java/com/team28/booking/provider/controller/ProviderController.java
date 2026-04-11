0312298f79f9e76ba2764b9dbda1d5b59358ae41
Abdelrahman <abdelrahmanhamada65@gmail.com>
2026-04-11 15:45:56 +0200

    feat(provider-service): added provider rating (68-1664)

--- provider-service/src/main/java/com/team28/booking/provider/controller/ProviderController.java
+++ provider-service/src/main/java/com/team28/booking/provider/controller/ProviderController.java
@@ -1,5 +1,6 @@
 package com.team28.booking.provider.controller;
 
+import com.team28.booking.provider.dto.RateProviderDTO;
 import com.team28.booking.provider.dto.VerifiedBy;
 import com.team28.booking.provider.model.Provider;
 import com.team28.booking.provider.service.ProviderService;
@@ -51,6 +52,13 @@ public class ProviderController {
             @RequestParam(required = false) Provider.ProviderStatus status
     ) {
         return providerService.filterByPricingTier(tier, status);
+    }
+
+    @PostMapping("/{id}/rate")
+    public void rateProvider(
+        @PathVariable Long id, @RequestBody RateProviderDTO rateProvider
+    ) {
+        providerService.rateProvider(id, rateProvider);
     }
 
     @PutMapping("/{providerId}/certifications/{certificationId}/verify")
