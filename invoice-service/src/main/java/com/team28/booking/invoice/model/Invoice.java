d42a4d378cae1d5cd8b3a8d1cb8dadd207063270
Abdelrahim Abdelazim <abdelrahimabdelazim303@gmail.com>
2026-04-11 10:44:18 +0200

    fix(invoice-service): fix JsonIgnore, HTTP methods, and exception consistency (55-24423)

--- invoice-service/src/main/java/com/team28/booking/invoice/model/Invoice.java
+++ invoice-service/src/main/java/com/team28/booking/invoice/model/Invoice.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -52,6 +53,7 @@ public class Invoice {
     private LocalDateTime createdAt;
 
     // relational attribute
+    @JsonIgnore
     @OneToMany(mappedBy = "invoice", cascade = CascadeType.ALL)
     private List<InvoiceDiscount> invoiceDiscounts = new ArrayList<>();
 
