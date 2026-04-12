0f70cca102c168be7ef5918c1eaba147be6e2066
TheSant0x <mahmoud7103@gmail.com>
2026-04-12 23:54:22 +0200

    fix(user-service): extract user status enum (55-24394)

---
+++ user-service/src/main/java/com/team28/booking/user/model/Status.java
@@ -0,0 +1,6 @@
+package com.team28.booking.user.model;
+
+public enum Status {
+    ACTIVE,
+    DEACTIVATED
+}
--- user-service/src/main/java/com/team28/booking/user/model/User.java
+++ user-service/src/main/java/com/team28/booking/user/model/User.java
@@ -52,10 +52,6 @@ public class User {
     @JsonManagedReference
     private List<SavedAddress> savedAddresses = new ArrayList<>();
 
-    public enum Status {
-        ACTIVE, DEACTIVATED
-    }
-
     // Constructors
     public User() {
     }
