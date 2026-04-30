f0a24ea0df40433e66e1a9d97330079fbab40e10
AhmedKamal18 <ahmedelbakly95@gmail.com>
2026-04-30 18:46:01 +0300

    feat(user): MOD-1 hash passwords with BCrypt and hide field in DTO (55-23815)

--- user-service/src/main/java/com/team28/booking/user/model/User.java
+++ user-service/src/main/java/com/team28/booking/user/model/User.java
@@ -1,5 +1,6 @@
 package com.team28.booking.user.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import org.hibernate.annotations.JdbcTypeCode;
@@ -24,6 +25,7 @@ public class User {
     @Column(nullable = false, unique = true)
     private String email;
 
+    @JsonIgnore
     @Column(nullable = false)
     private String password;
 
