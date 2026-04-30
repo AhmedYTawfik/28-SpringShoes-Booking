275c4231a639981a8ef134bd482cd01aa6619a56
Abdelrahim Abdelazim <abdelrahimabdelazim303@gmail.com>
2026-04-30 17:32:11 +0300

    Revert "feat(cc): DP-6 introduce MongoEvent interface, EventType enum, and Ev…"

--- invoice-service/src/main/java/com/team28/booking/invoice/factory/EventType.java
+++
@@ -1,5 +0,0 @@
-package com.team28.booking.invoice.factory;
-
-public enum EventType {
-    AUTH, PROVIDER, BOOKING, CALENDAR, PAYMENT_AUDIT
-}
--- user-service/src/main/java/com/team28/booking/user/factory/EventType.java
+++
@@ -1,5 +0,0 @@
-package com.team28.booking.user.factory;
-
-public enum EventType {
-    AUTH, PROVIDER, BOOKING, CALENDAR, PAYMENT_AUDIT
-}
